671280443e5a79910561cbeadfac71e5f6d3b334
Daniel Choi <cys920622@gmail.com>
2015-11-14 12:44:03 -0800

    fb login button added with form that doesn't do anything yet

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '789309501195621',
+    secret: '7bce4437c608f8d99450bcad2dd83133'
+});
