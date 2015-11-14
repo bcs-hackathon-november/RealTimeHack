047ce917ccd87c4ee51361309285481624d29a00
Daniel Choi <cys920622@gmail.com>
2015-11-14 14:50:55 -0800

    implement add and remove from online users but with dummy names

--- client/login/login.js
+++ client/login/login.js
@@ -1,51 +1,39 @@
-<<<<<<< HEAD
-UserList = new Mongo.Collection('usersList');
+Template.exampleTemplate.helpers({
+    'getName': function () {
+        var userName = "Dan";
+        return userName;
+    }
+});
 
-    Template.login.helpers({
-        'getName': function () {
-            var userName = "Dan";
-            return userName;
-        }
-    });
-
-    Template.login.events({
-        'click #facebook-login': function (event) {
-            Meteor.loginWithFacebook({}, function (err) {
-                if (err) {
-                    throw new Meteor.Error("Facebook login failed");
-                }
-            });
-            //TODO
-            //var usrName = Meteor.user().username;
-            var usrName = "Example name";
-            UserList.insert({name: usrName});
-
-        },
-
-        'click #logout': function (event) {
-            Meteor.logout(function (err) {
-                if (err) {
-                    throw new Meteor.Error("Logout failed");
-                }
-            });
-            //TODO
-            UserList.remove(UserList.find(Meteor.user()._id));
-        }
-    });
-=======
-window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '789309501195621',
-            xfbml      : true,
-            version    : 'v2.5'
+Template.login.events({
+    'click #facebook-login': function (event) {
+        Meteor.loginWithFacebook({}, function (err) {
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
         });
-};
+        //TODO
+        //var usrName = Meteor.user().username;
 
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
->>>>>>> 268fc1711dfec86fe927bd62f4b9408ec11fb622
+
+
+        var usrName = "Example 777";
+
+        var usrId = UserList.insert({'name': usrName});
+        Session.set('thisUser', usrId);
+        console.log(usrId);
+
+    },
+
+    'click #logout': function (event) {
+        Meteor.logout(function (err) {
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        });
+        //TODO
+        var usrToRm = Session.get('thisUser');
+        console.log(usrToRm);
+        UserList.remove(usrToRm);
+    }
+});
